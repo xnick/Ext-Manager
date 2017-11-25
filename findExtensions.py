4661bb5851656e60076c627a293b1e9af59813b9
EvanDimitriadis <33984704+EvanDimitriadis@users.noreply.github.com>
2017-11-25 21:35:56 +0200

    findDomains

--- findExtensions.py
+++ findExtensions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 import glob
-
+from urllib.parse import urlparse
 from extension import Extension
 
 def findLinks(filename):
@@ -9,7 +9,12 @@ def findLinks(filename):
         output=file.read()
         urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', output)
         return(urls)
-        
+
+def findDomains(filename):
+    urls = findLinks(filename)
+    domains = [urlparse(url)[1].split('/') for url in urls]
+    return(domains)
+
 if __name__ == '__main__':
     if os.name=="posix":
         path=os.path.expanduser("~/.config/google-chrome/Default/Extensions/*")
